b1427a3f68a24c9b43b854cc48dca773b8c071e2
tilnow <44685516+tilnow@users.noreply.github.com>
2022-04-29 11:34:04 +0300

    debugging v2
    why no response to $gigtest

--- gigayak.py
+++ gigayak.py
@@ -86,7 +86,7 @@ async def on_message(message):
 		
     dmtarget=await dmchan(message.author.id,message.channel) #build backchannel to user, so we do not answer in general channel
     ###ISSUE: does this for any message on server, does not check if a bot request
-
+    print("now ready to try various bots:"+message.content)
 #three bots that manage general lists
 #gigabot
     await try_bot("gig",message)
